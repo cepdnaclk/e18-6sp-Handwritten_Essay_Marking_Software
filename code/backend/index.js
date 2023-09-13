c912e8ba2126d30f254372654f966839cd184688
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2023-09-13 17:26:54 +0530

    backend initiate

---
+++ code/backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const multer = require('multer');
+const uploadFile = require('./routes/fileUpload')
+const cors = require("cors");
+
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
